35a42ddb4d6acfc80c01bab28a8769fdcd40b11e
Alex Lee <hyunho98@gmail.com>
2019-10-28 00:39:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[3].insert(element)
+end
+
+def using_uniq(array)
+  array.using_uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
